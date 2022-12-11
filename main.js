bf8da0e8c829d5889536708978b741b07aaee1f5
mahmut-31 <mahmut-31@gmail.com>
2022-12-11 17:47:42 +0300

    first commmit

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-- -asdsasd dasdsad
-- -asdsasd klg mdfkjgn
-- -asdsasd dflkgmlfdk
-- -asdsasd dfplgöş
-- -asdsasd ShadowRoot
-asdsasd asdadafhfh
-- -asdsasd DataTransfer
-- -asdsasd DataTransfer
-- -asdsasd DataTransfer
-- -asdsasd DataTransfer
-- -asdsasd DataTransfer
-- -asdsasd DataTransfer
-- -asdsasd DataTransfer
+# Basliklar -> h1
+## -> h2
+### -> h3
+
+- list item 1
+- list item 2
+
+* list item 1 [link](https://kodluyoruz.org)
+* list item 2
+
+*italic* **bold** ***bolditalic***
+
+[Google Gitmek icin Tiklayiniz](https://google.com)
+
+***
+
+
+![Lorem Picsum Gorsel](https://picsum.photos/200/300)
+
+***
+
+```python
+print("hello world")
+```
+
+```javascript
+console.log("hello world")
+
+function helloWorld() {
+    console.log("hello world")
+}
+```
